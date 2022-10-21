9b0dfdbb2a1d5e1e50ed410e9acd39594d09c842
CoolComicsJ <ziggaton@gmail.com>
2022-10-20 22:25:45 -0500

    "Added filewrite"

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Read-Me-Creator
+# Cheese Pizza Readme 
+    
+# Table of Contents
+    
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Test Instructions](#test)
+5. [Contributions](#contributions)
+6. [Questions](#questions)
+7. [License](#license)
+    
+## Description <a name="description"></a>
+    
+A cheese pizza is a pie made of 4 different cheeses, marinara sauce, and dough baked in an oven.
+    
+## Installation Instructions <a name="installation"
+    
+To eat the pizza cut it into 8 slices and bite it. Wait for it to cool.
+    
+## Usage <a name="usage"></a>
+    
+Eat cheese pizza to sate your hunger and fulfill your cravings. Just dont eat too much or you'll get sick or fat.
+    
+## Test Instructions <a name="test"></a>
+    
+When the crust is brown and cheese is bubbling.
+    
+## Contributors<a name="contributions"></a>
+    
+Pizza Hut, Little Ceasars, Dominoes.
+    
+## Questions <a ="questions"></a>
+    
+To contact me please refer to the following links
+mario@superpizzabros.com
+github.com/mario
+    
+## License
+    
+MIT License
--- index.js
+++ index.js
@@ -2,6 +2,53 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
+const generateReadMe = ({projectName, description, installation, licenseChoice, usage, contributions, testInstructions, gitHuburl, email}) => 
+
+`# ${projectName} Readme 
+    
+# Table of Contents
+    
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Test Instructions](#test)
+5. [Contributions](#contributions)
+6. [Questions](#questions)
+7. [License](#license)
+    
+## Description<a name="description"></a>
+    
+${description}
+    
+## Installation Instructions<a name="installation"></a>
+    
+${installation}
+    
+## Usage <a name="usage"></a>
+    
+${usage}
+    
+## Test Instructions<a name="test"></a>
+    
+${testInstructions}
+    
+## Contributors<a name="contributions"></a>
+    
+${contributions}
+    
+## Questions<a name="questions"></a>
+    
+To contact me please refer to the following links
+${email}
+${gitHuburl}
+    
+## License
+  
+
+${licenseChoice}`
+
+
+
 inquirer.prompt([
     {
         type: 'input',
@@ -38,7 +85,22 @@ inquirer.prompt([
         type: 'input',
         name: 'testInstructions',
         message: 'Write out how your application should be tested for functionality.',
+    },
+    {
+        type: 'input',
+        name: 'gitHuburl',
+        message: 'Provide your GitHub profile URL'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Lastly, give your E-Mail Address'
     }
-]).then((data) => {
-    console.log(data)
+]).then((answers) => {
+    console.log(answers)
+    const readMeAnswers = generateReadMe(answers);
+
+    fs.writeFile('readme.md', readMeAnswers, (err) =>
+    err ? console.log(err) : console.log('Read Me Successfully Generated!')
+    )
 })
